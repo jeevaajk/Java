927df6ef9c03b55ded348d03f6168ff8611d1ad9
Jeevaa JK <jeevaajk2007@gmail.com>
2025-09-14 23:29:18 +0530

    Add files via upload
    A Simple swapping Technique

---
+++ Swap.java
@@ -0,0 +1,14 @@
+public class Swap
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=10;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("The value of a is "+a);
+        System.out.println("The value of b is "+b);
+
+    }
+}
